2374a34cf17d0ebeeefa6bbd5ee6940e777e3ac9
borehelu <borehelu@gmail.com>
2023-02-16 16:50:24 +0300

    initial backend setup

--- backend/routes/api/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createUser } = require("../../controller/userController");
-
-const router = express.Router();
-
-router.post("/create-user", createUser);
-
-module.exports = router;
