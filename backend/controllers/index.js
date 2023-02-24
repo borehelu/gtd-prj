ca7646d3f2f0f0d60e40950b60dc315697dc1e42
borehelu <borehelu@gmail.com>
2023-02-24 14:27:09 +0300

    create tasks and get tasks

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,5 +1,6 @@
 const UserController = require("./userController");
 const InboxController = require("./inboxController");
 const ProjectsController = require('./projectsController');
+const TasksController = require('./tasksController')
 
-module.exports = { UserController, InboxController,ProjectsController }
+module.exports = { UserController, InboxController,ProjectsController,TasksController }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRoute = require('./api/user');
 const inboxRoute = require('./api/inbox');
 const projectRoute = require('./api/projects');
+const tasksRoute = require('./api/tasks');
 
 const router = express.Router();
 
 router.use('/auth', userRoute);
 router.use('/inbox',inboxRoute);
 router.use('/projects',projectRoute);
+router.use('/tasks',tasksRoute);
 
 module.exports = router;
