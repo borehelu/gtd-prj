cca328e8dfe71a0d5df21c6b1ba5fecc35e0e9e3
borehelu <borehelu@gmail.com>
2023-02-28 13:30:47 +0300

    add endpoints for project-tasks

--- backend/routes/api/projects.js
+++ backend/routes/api/projects.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { verifyJWT, validate } = require("../../middlewares");
 const ProjectsController = require("../../controllers/projectsController");
+const ProjectTasksController = require("../../controllers/projectTasksController");
 
 const router = express.Router();
 const { createProject, getProject, getProjects, updateProject, deleteProject } =
@@ -11,5 +12,25 @@ router.get("/:id", verifyJWT, getProject);
 router.get("/", verifyJWT, getProjects);
 router.put("/:id", verifyJWT, validate("createProjects"), updateProject);
 router.delete("/:id", verifyJWT, deleteProject);
+
+router.post(
+  "/:id/tasks",
+  verifyJWT,
+  validate("createProjectTasks"),
+  ProjectTasksController.createTask
+);
+router.get("/:id/tasks/:task_id", verifyJWT, ProjectTasksController.getTask);
+router.get("/:id/tasks", verifyJWT, ProjectTasksController.getTasks);
+router.put(
+  "/:id/tasks/:task_id",
+  verifyJWT,
+  validate("createProjectTasks"),
+  ProjectTasksController.updateTask
+);
+router.delete(
+  "/:id/tasks/task_id",
+  verifyJWT,
+  ProjectTasksController.deleteTask
+);
 
 module.exports = router;
