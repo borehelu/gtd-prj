5f0e322ab2b0deed1482603269282c3667a98b48
borehelu <borehelu@gmail.com>
2023-03-08 00:36:20 +0300

    add logout hook

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,12 +1,16 @@
 import axios from "../utils/axios";
 import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const useLogout = () => {
   const { setAuth } = useAuth();
+  const navigate = useNavigate();
   const logout = async () => {
-    setAuth({});
+    
     try {
-      const response = await axios("/logout", { withCredentials: true });
+      const response = await axios("auth/logout", { withCredentials: true });
+      setAuth({});
+      navigate('/login');
     } catch (error) {
       console.error(error);
     }
