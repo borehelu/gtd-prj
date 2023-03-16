1453cd6c65e740ef53e4e8ac0c7cf57dc37f4205
borehelu <borehelu@gmail.com>
2023-03-16 22:22:43 +0300

    modify forgot pwd page to new design

--- frontend/src/pages/ForgotPassword/styles.js
+++ frontend/src/pages/ForgotPassword/styles.js
@@ -1,27 +1,39 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #f8f8f8;
+  background-color: #f7f7f7;
+
+  & main {
+    width: 90%;
+    height: 55%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    border-radius: 0.4rem;
+    box-shadow: 1px 3px 5px -1px rgba(0, 0, 0, 0.1);
+    padding: 0;
+  }
 `;
 
 export const Form = styled.form`
-  padding: 1rem 2rem;
-  background-color: #fff;
-  border-radius: .4rem;
-  box-shadow: 1px 3px 5px -1px rgba(0,0,0,0.1);
-
   & p {
     font-size: 0.925rem;
-    margin-bottom: 1rem;
+    max-width: 36ch;
   }
-  & a {
-    color: #333;
+
+  & h2 {
+    font-size: 1.3rem;
+  }
+  & .forgot_password_link {
+    display: block;
     font-size: 0.925rem;
+    margin-top: 1rem;
+    text-align: right;
+    color: var(--primary-color);
   }
 `;
 
@@ -30,11 +42,50 @@ export const Button = styled.button`
   background: none;
   border: none;
   padding: 0.375rem;
-  background-color: #002e4e;
+  background-color: var(--primary-color);
   color: #fff;
   width: 250px;
   border-radius: 0.125rem;
-  margin: 0.5rem 0;
+  margin: 1.5rem 0;
   cursor: pointer;
   font-weight: 700;
+`;
+
+export const LeftPanel = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  & p {
+    font-size: 0.925rem;
+
+    & a {
+      color: var(--primary-color);
+      font-weight: 700;
+    }
+  }
+`;
+
+export const RightPanel = styled.div`
+  background-color: var(--primary-color);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  padding: 2rem 1rem;
+
+  & h2 {
+    font-size: 1.25rem;
+  }
+
+  & p {
+    font-size: 0.925rem;
+  }
+
+  & img {
+    height: 60%;
+  }
 `;
