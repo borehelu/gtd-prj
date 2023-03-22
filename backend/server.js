8748a7345178acae9677de01ac0fd8580379f9db
borehelu <borehelu@gmail.com>
2023-03-22 21:34:17 +0300

    add cors origin for localhost

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const createError = require("http-errors");
 
 const app = express();
 
-app.use(cors({credentials:true}));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
