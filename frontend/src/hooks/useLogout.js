6c2c88cafb525f4a62ea87119cd76d383dda2aa3
borehelu <borehelu@gmail.com>
2023-03-22 21:32:23 +0300

    implement logout function

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,16 +1,14 @@
 import axios from "../utils/axios";
 import useAuth from "./useAuth";
-import { useNavigate } from "react-router-dom";
 
 const useLogout = () => {
   const { setAuth } = useAuth();
-  const navigate = useNavigate();
+
   const logout = async () => {
-    
     try {
-      const response = await axios("auth/logout", { withCredentials: true });
       setAuth({});
-      navigate('/login');
+      const response = await axios("auth/logout", { withCredentials: true });
+      console.log(response);
     } catch (error) {
       console.error(error);
     }
