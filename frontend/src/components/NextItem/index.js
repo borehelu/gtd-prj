7ffe65e904d97bd8f0e8229a39a76565da89e5e5
borehelu <borehelu@gmail.com>
2023-03-29 14:15:22 +0300

    refactor nextitem component

--- frontend/src/components/NextItem/index.js
+++ frontend/src/components/NextItem/index.js
@@ -1,94 +1,59 @@
-import React from "react";
-import { Card } from "./styles";
-import { IoEllipsisVertical } from "react-icons/io5";
+import { IoEllipsisVertical, IoTimeOutline } from "react-icons/io5";
+import { formatDate } from "../../utils/date";
+import Options from "../Options/";
+import useNextActions from "../../hooks/useNextActions";
+import { useEffect } from "react";
+import {
+	NextItemContainer,
+	Heading,
+	Footer,
+	Name,
+	Description,
+	Status,
+	Priority,
+	DueDate,
+} from "./styles";
 
 function NextItem({ item }) {
+	const { removeItem, setSelected, setShowEditing } = useNextActions();
+
 	return (
-		<Card>
-			<div className="top">
-				<p className="item_name">{item.item_name}</p>
-				<div className="status pending">
+		<NextItemContainer>
+			<Heading>
+				<Name>{item.item_name}</Name>
+				<Status className={`${formatStatus(item.status)}`}>
 					<div className="dot"></div>
-					<p>{item.status}</p>
-				</div>
-				<div className="action">
-					<IoEllipsisVertical className="icon" />
-				</div>
-			</div>
-			<p className="item_description">{item.description}</p>
-			<div className="bottom">
-				<div className="chip context">{item.context}</div>
-				<div className="chip priority">{item.priority}</div>
-				<div className="chip deadline">{item.due_date}</div>
-			</div>
-		</Card>
+					{item.status}
+				</Status>
+				<Options
+					item={item}
+					removeItem={removeItem}
+					setSelected={() => setSelected(item)}
+					setShowEditing={setShowEditing}
+				/>
+			</Heading>
+
+			<Description>{item.description}</Description>
+
+			<DueDate>
+				<IoTimeOutline />
+				in {formatDate(new Date(item.due_date))}
+			</DueDate>
+		</NextItemContainer>
 	);
 }
 
+function formatStatus(status) {
+	switch (status) {
+		case "Pending":
+			return "pending";
+		case "In Progress":
+			return "inprogress";
+		case "Complete":
+			return "completed";
+		default:
+			return "";
+	}
+}
+
 export default NextItem;
-
-// <div className="card next_action">
-//   <div className="top">
-//     <p className="item_name">Call Beth</p>
-//     <div className="status pending">
-//       <div className="dot"></div>
-//       <p>Pending</p>
-//     </div>
-//     <div className="action">
-//       <IoEllipsisVertical className="icon" />
-//     </div>
-//   </div>
-//   <p className="item_description">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed,
-//     accusamus.
-//   </p>
-//   <div className="bottom">
-//     <div className="context chip">@Home</div>
-//     <div className="chip priority">High</div>
-//     <div className="chip deadline">In 3 weeks</div>
-//   </div>
-// </div>
-
-// <div className="card next_action">
-//   <div className="top">
-//     <p className="item_name">Call Beth</p>
-//     <div className="status completed">
-//       <div className="dot"></div>
-//       <p>Completed</p>
-//     </div>
-//     <div className="action">
-//       <IoEllipsisVertical className="icon" />
-//     </div>
-//   </div>
-//   <p className="item_description">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed,
-//     accusamus.
-//   </p>
-//   <div className="bottom">
-//     <div className="context chip">@Home</div>
-//     <div className="chip priority">High</div>
-//     <div className="chip deadline">In 1 hour</div>
-//   </div>
-// </div>
-
-// <div className="card next_action">
-//   <div className="top">
-//     <p className="item_name">Call Beth</p>
-//     <div className="status inprogress">
-//       <div className="dot"></div>
-//       <p>In progress</p>
-//     </div>
-//     <div className="action">
-//       <IoEllipsisVertical className="icon" />
-//     </div>
-//   </div>
-//   <p className="item_description">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed,
-//     accusamus.
-//   </p>
-//   <div className="bottom">
-//     <div className="context chip">@Home</div>
-//     <div className="chip priority">High</div>
-//     <div className="chip deadline">In 1 hour</div>
-//   </div>
-// </div>
