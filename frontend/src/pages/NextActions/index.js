6a056789ee492305f0cb2cab0c226eecce0ea009
borehelu <borehelu@gmail.com>
2023-03-30 23:31:01 +0300

    update nextactions page

--- frontend/src/pages/NextActions/index.js
+++ frontend/src/pages/NextActions/index.js
@@ -1,70 +1,102 @@
-import {
-  IoGridOutline,
-  IoStopwatchOutline,
-  IoCheckmarkDoneOutline,
-  IoSearchOutline,
-  IoHourglassOutline,
-  IoChevronDownOutline,
-  IoOptionsOutline,
-  IoListOutline,
-  IoEllipsisVertical,
-} from "react-icons/io5";
-
-import { Main, Section, SearchBarForm } from "./styles";
-import SearchBar from "./SearchBar";
-import NextItem from "../../components/NextItem";
-
-const next = [
-  {
-    id: 1,
-    item_name: "Call Beth to plan the birthday party",
-    description: "We need Beths feedback on whether or not to buy a cake.",
-    status: "Pending",
-    priority: "Medium",
-    due_date: "In 3 weeks",
-    context: "@Home",
-  },
-  {
-    id: 2,
-    item_name: "Call Beth to plan the birthday party",
-    description: "We need Beths feedback on whether or not to buy a cake.",
-    status: "Pending",
-    priority: "Medium",
-    due_date: "In 3 weeks",
-    context: "@Home",
-  },
-  {
-    id: 3,
-    item_name: "Get the financials done",
-    description: "We need Beths feedback on whether or not to buy a cake.",
-    status: "Pending",
-    priority: "Medium",
-    due_date: "In 3 weeks",
-    context: "@Home",
-  },
-  {
-    id: 4,
-    item_name: "Call Beth to plan the birthday party",
-    description: "We need Beths feedback on whether or not to buy a cake.",
-    status: "Pending",
-    priority: "Medium",
-    due_date: "In 3 weeks",
-    context: "@Home",
-  },
-];
+import toast, { Toaster } from "react-hot-toast";
+import { Main } from "./styles";
+import SearchBar from "../../components/SearchBar";
+import NextActionsList from "./NextActionsList";
+import EditNextAction from "./EditNextAction";
+import ActionsBar from "./ActionsBar";
+import { useState, useReducer, useEffect } from "react";
+import useFilter from "../../hooks/useFilter";
+import useApi from "../../hooks/useApi";
+import usePagination from "../../hooks/usePagination";
+import Pagination from "../../components/Pagination";
+import { IoListOutline } from "react-icons/io5";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import { handleFilter, handleSearch, checkFilters } from "./helper";
 
 function NextActions() {
+  const { state, createItem, updateItem, removeItem } = useApi("next-actions/");
+  const [active, setActive] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filterParams, dispatch] = useFilter();
+
+  // const { handlePageChange, currentPage, currentData, setCurrentData } =
+  //   usePagination(filteredData, 5);
+
+  // useEffect(() => {
+  //   if (searchQuery) {
+  //     setCurrentData(handleSearch(state.data, searchQuery));
+  //   } else {
+  //     setCurrentData(state.data);
+  //   }
+  // }, [searchQuery]);
+
+  // useEffect(() => {
+  //   if (checkFilters(filterParams)) {
+  //     setFilteredData(handleFilter(state.data, filterParams));
+  //   } else {
+  //     setFilteredData(currentData);
+  //   }
+  // }, [filterParams]);
+
+  const handleUpdate = async (id, updatedItem) => {
+    const toastId = toast.loading("Updating...");
+    try {
+      await updateItem(id, updatedItem);
+      toast.success("Item updated", { id: toastId });
+    } catch (error) {
+      toast.error("Error updating item", { id: toastId });
+    } finally {
+      setIsEditing(false);
+    }
+  };
+
+  const handleDelete = (id) => {
+    const toastId = toast.loading("Deleting...");
+    try {
+      removeItem(id);
+      toast.success("Item deleted!", { id: toastId });
+    } catch (e) {
+      toast.error(e.message, { id: toastId });
+    }
+  };
+
   return (
     <Main>
-      <SearchBar />
-      <Section>
-        <h2 className="section_heading">Next Actions</h2>
-        {next.map((item) => (
-          <NextItem key={item.id} item={item} />
-        ))}
-      </Section>
+      <Toaster />
+      <SearchBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        placeholder="Search next actions ..."
+        icon={<IoListOutline className="icon" />}
+      />
+      <EditNextAction
+        onUpdate={handleUpdate}
+        active={active}
+        isEditing={isEditing}
+        setIsEditing={setIsEditing}
+      />
+      <ActionsBar filterParams={filterParams} dispatch={dispatch} />
+
+      {state.loading ? (
+        <LoadingSpinner />
+      ) : (
+        <NextActionsList
+          data={state.data}
+          onDelete={handleDelete}
+          setActive={setActive}
+          setIsEditing={setIsEditing}
+        />
+      )}
     </Main>
   );
 }
 
 export default NextActions;
+
+// <Pagination
+//   itemsPerPage={5}
+//   totalItems={state.data.length}
+//   currentPage={currentPage}
+//   onPageChange={handlePageChange}
+// />
--- frontend/src/pages/NextActions/styles.js
+++ frontend/src/pages/NextActions/styles.js
@@ -5,84 +5,26 @@ export const Main = styled.main`
   height: 100vh;
 `;
 
-export const Wrapper = styled.div`
-  min-width: 100vw;
-  min-height: 20%;
-  margin: 1rem 0;
-  background-color: #f7f7f7;
-  border-radius: 1rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+export const Section = styled.section`
+  width: 80%;
+  max-width: 600px;
+  margin: 1rem auto;
 `;
 
-export const SearchBarForm = styled.form`
-  width: 60%;
-  background-color: #fff;
-  border-radius: 1rem;
-  padding: 0.7rem 1.5rem;
-  box-shadow: 0 2px 15px -5px rgba(0, 0, 0, 0.1);
+export const SectionHeading = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
-
-  & .form-control {
-    display: flex;
-    align-items: center;
-    & .icon {
-      color: #999;
-      font-size: 1.3rem;
-    }
-    & input {
-      border: none;
-      outline: none;
-      padding: 1rem 1.5rem;
-
-      &:focus {
-        outline: none;
-      }
-    }
-  }
-
-  & .primary {
-    width: 120px;
-    background-color: var(--primary-color);
-    color: var(--white);
-    padding: 0.6rem 1.5rem;
-    border-radius: 0.6rem;
-    font-size: 0.9rem;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    font-weight: 500;
-
-    & .icon {
-      font-size: 0.9rem;
-    }
-  }
 `;
 
-export const Section = styled.section`
-  width: 60%;
-  margin: 1rem auto;
-
-  & .heading {
-    display: flex;
-    justify-content: space-between;
-
-    & button {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      column-gap: 0.5rem;
-      background-color: #f7f7f7;
-      color: #777;
-      padding: 0.5rem 1rem;
-      border-radius: 0.4rem;
-    }
-  }
+export const ActionButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 0.5rem;
+  background-color: #f7f7f7;
+  color: #777;
+  padding: 0.2rem 1rem;
+  border-radius: 0.4rem;
 `;
 
 export const ButtonGroup = styled.div`
@@ -104,4 +46,185 @@ export const ButtonGroup = styled.div`
   }
 }
 
+`;
+
+export const NewContext = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  background-color: #f7f7f7;
+  padding: 0.5rem 1rem;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+
+  & .top {
+    display: flex;
+    justify-content: space-between;
+    color: #555;
+    font-size: 0.9rem;
+
+    & button {
+      font-size: 1.25rem;
+    }
+  }
+
+  & .context-form {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    margin-bottom: 0.5rem;
+    height: 0;
+    overflow: hidden;
+    transition: height 4s ease-in-out;
+  }
+
+  & .context-form.show {
+    height: auto;
+  }
+  & input {
+    width: 60%;
+    height: 40px;
+    border-radius: 4px;
+    background-color: #f7f7f7;
+    font-size: 14px;
+    display: block;
+    border: none;
+    border-radius: 2px;
+    padding: 0.5rem 1rem;
+    border-bottom: 2px solid var(--primary-color);
+
+    &:focus {
+      outline: none;
+      border-bottom: 2px solid var(--primary-color);
+    }
+  }
+
+  & button {
+    color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: 0.5rem;
+    font-size: 0.9rem;
+    border-radius: 4px;
+  }
+`;
+
+export const FilterContainer = styled.div`
+  overflow-y: scroll;
+  display: flex;
+  column-gap: 0.5rem;
+  padding: 1rem 0;
+`;
+
+export const FilterChip = styled.div`
+  border: 2px solid #888;
+  font-size: 14px;
+  background-color: #f7f7f7;
+  color: #444;
+  padding: 4px 10px;
+  display: grid;
+  grid-template-columns: auto 1.2rem;
+  align-items: center;
+  column-gap: 8px;
+  border-radius: 1.5rem;
+
+  & svg {
+    font-size: 1.2rem;
+    cursor: pointer;
+  }
+`;
+
+export const NextItemContainer = styled.div`
+  background-color: var(--white);
+  margin: 1rem 0;
+  border: 1px solid #efefef;
+  box-shadow: 0 2px 3px -2px rgba(0, 0, 0, 0.15),
+    2px 0 3px -2px rgba(0, 0, 0, 0.15);
+  border-radius: 6px;
+  padding: 0.7rem 1rem;
+`;
+
+export const Heading = styled.div`
+  display: flex;
+  align-items: flex-end;
+  column-gap: 1rem;
+  margin-bottom: 0.2rem;
+`;
+
+export const Name = styled.h2`
+  font-size: 0.975rem;
+  color: #444;
+  font-weight: 600;
+  max-width: 20ch;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const Description = styled.p`
+  font-size: 0.925rem;
+  color: #555;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 0.5rem;
+`;
+
+export const Priority = styled.div`
+  font-size: 0.6rem;
+`;
+
+export const Status = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 0.3rem;
+  font-size: 0.8rem;
+  padding: 1px 8px;
+  & .dot {
+    background-color: #ddd;
+    width: 4px;
+    height: 4px;
+    border-radius: 6px;
+  }
+  &.completed {
+    background-color: var(--completed-light);
+    color: var(--completed);
+
+    & .dot {
+      background-color: var(--completed);
+    }
+  }
+
+  &.inprogress {
+    background-color: var(--in-progress-light);
+    color: var(--in-progress);
+
+    & .dot {
+      background-color: var(--in-progress);
+    }
+  }
+
+  &.pending {
+    background-color: var(--pending-light);
+    color: var(--pending);
+
+    & .dot {
+      background-color: var(--pending);
+    }
+  }
+`;
+
+export const DueDate = styled.div`
+  margin-top: 0.7rem;
+  display: inline-flex;
+  align-items: center;
+  column-gap: 0.3rem;
+  font-size: 0.725rem;
+  background-color: var(--pending-light);
+  padding: 2px 8px;
+  border-radius: 1rem;
+  color: #555;
 `;
