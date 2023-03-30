21c25000b301f62bece818e765503ed45914e01e
borehelu <borehelu@gmail.com>
2023-03-30 23:32:54 +0300

    setup basic structure of references component

---
+++ frontend/src/pages/References/EditReference.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditReference() {
+	return <div></div>;
+}
+
+export default EditReference;
--- frontend/src/pages/References/index.js
+++ frontend/src/pages/References/index.js
@@ -1,5 +1,32 @@
+import { useState, useReducer, useEffect } from "react";
+import useFilter from "../../hooks/useFilter";
+import { Toaster } from "react-hot-toast";
+import { Main } from "./styles";
+import SearchBar from "../../components/SearchBar";
+import ReferenceList from "./ReferenceList";
+import EditReference from "./EditReference";
+import { IoDocumentTextOutline } from "react-icons/io5";
+
 function References() {
-  return <main>References</main>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filterParams, dispatch] = useFilter();
+  return (
+    <Main>
+      <Toaster />
+      <SearchBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        placeholder="Search references..."
+        icon={<IoDocumentTextOutline className="icon" />}
+      />
+      <EditReference />
+      <ReferenceList
+        searchQuery={searchQuery}
+        filterParams={filterParams}
+        dispatch={dispatch}
+      />
+    </Main>
+  );
 }
 
 export default References;
