0fef2dcb3b6ef145db76547bac966ef4f569976a
borehelu <borehelu@gmail.com>
2023-03-30 23:28:32 +0300

    refactor options component for reuse

--- frontend/src/components/Options/index.js
+++ frontend/src/components/Options/index.js
@@ -8,23 +8,17 @@ import {
 	IoArrowRedoOutline,
 } from "react-icons/io5";
 
-function Options({ item, removeItem, setSelected, setShowEditing }) {
+function Options({ item, onDelete, setActive, setIsEditing }) {
 	const [show, setShow] = useState(false);
 
 	const handleEdit = () => {
 		setShow(false);
-		setSelected(item);
-		setShowEditing(true);
+		setActive(item);
+		setIsEditing(true);
 	};
 	const handleDelete = () => {
 		setShow(false);
-		const toastId = toast.loading("Deleting...");
-		try {
-			removeItem(item.id);
-			toast.success("Item deleted!", { id: toastId });
-		} catch (e) {
-			toast.error(e.message, { id: toastId });
-		}
+		onDelete(item.id);
 	};
 
 	return (
