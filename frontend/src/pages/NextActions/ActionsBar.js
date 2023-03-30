1982772d92f0b369204f86467ab2acc2a55a95cd
borehelu <borehelu@gmail.com>
2023-03-30 23:16:48 +0300

    implement actions bar

---
+++ frontend/src/pages/NextActions/ActionsBar.js
@@ -0,0 +1,28 @@
+import FilterModal from "./FilterModal";
+import FilterBar from "./FilterBar";
+import { Section, SectionHeading, ActionButton } from "./styles";
+import { IoOptionsOutline, IoCloseCircleOutline } from "react-icons/io5";
+import { useState } from "react";
+
+function ActionsBar({ dispatch, filterParams }) {
+	const [showFilter, setShowFilter] = useState(false);
+	return (
+		<Section>
+			<FilterModal
+				showFilter={showFilter}
+				setShowFilter={setShowFilter}
+				dispatch={dispatch}
+			/>
+			<SectionHeading>
+				<h2>Next Actions</h2>
+				<ActionButton onClick={() => setShowFilter(true)}>
+					<IoOptionsOutline />
+				</ActionButton>
+			</SectionHeading>
+
+			<FilterBar dispatch={dispatch} filterParams={filterParams} />
+		</Section>
+	);
+}
+
+export default ActionsBar;
