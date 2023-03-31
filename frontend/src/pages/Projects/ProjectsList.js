a28c40c31e53a4e0e73e892d171eaa5290d503b5
borehelu <borehelu@gmail.com>
2023-03-31 21:56:11 +0300

    implement project list component

--- frontend/src/pages/Projects/ProjectsList.js
+++ frontend/src/pages/Projects/ProjectsList.js
@@ -1,7 +1,37 @@
-import React from "react";
+import Empty from "../../components/Empty";
+import { Section } from "./styles";
+import usePagination from "../../hooks/usePagination";
+import Pagination from "../../components/Pagination";
+import Project from "./Project";
 
-function ProjectsList() {
-	return <div></div>;
+function ProjectsList({ data, onDelete, setActive, setIsEditing }) {
+	const { handlePageChange, currentPage, currentData, setCurrentData } =
+		usePagination(data, 5);
+
+	return (
+		<Section>
+			{currentData.length > 0 ? (
+				currentData.map((item) => (
+					<Project
+						key={item.id}
+						item={item}
+						onDelete={onDelete}
+						setActive={setActive}
+						setIsEditing={setIsEditing}
+					/>
+				))
+			) : (
+				<Empty />
+			)}
+
+			<Pagination
+				itemsPerPage={5}
+				totalItems={data.length}
+				currentPage={currentPage}
+				onPageChange={handlePageChange}
+			/>
+		</Section>
+	);
 }
 
 export default ProjectsList;
