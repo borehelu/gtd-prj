dec256d2f82e29bc979f366520917da98a2b46b4
borehelu <borehelu@gmail.com>
2023-03-31 21:53:55 +0300

    add project component

---
+++ frontend/src/pages/Projects/Project.js
@@ -0,0 +1,43 @@
+import { IoEllipsisVertical, IoTimeOutline } from "react-icons/io5";
+import { formatDate } from "../../utils/date";
+import formatStatus from "../../utils/status";
+import Options from "../../components/Options/";
+import {
+	NextItemContainer,
+	Heading,
+	Footer,
+	Name,
+	Description,
+	Status,
+	Priority,
+	DueDate,
+} from "./styles";
+
+function Project({ item, onDelete, setActive, setIsEditing }) {
+	return (
+		<NextItemContainer>
+			<Heading>
+				<Name>{item.project_name}</Name>
+				<Status className={`${formatStatus(item.status)}`}>
+					<div className="dot"></div>
+					{item.status}
+				</Status>
+				<Options
+					item={item}
+					setActive={setActive}
+					onDelete={onDelete}
+					setIsEditing={setIsEditing}
+				/>
+			</Heading>
+
+			<Description>{item.description}</Description>
+
+			<DueDate>
+				<IoTimeOutline />
+				in {formatDate(new Date(item.due_date))}
+			</DueDate>
+		</NextItemContainer>
+	);
+}
+
+export default Project;
