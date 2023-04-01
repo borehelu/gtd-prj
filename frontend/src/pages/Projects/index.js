74f8e604417d67eaa2bb40784862fec50da8848d
borehelu <borehelu@gmail.com>
2023-04-01 22:45:56 +0300

    add index page

--- frontend/src/pages/Projects/index.js
+++ frontend/src/pages/Projects/index.js
@@ -1,15 +1,45 @@
-import { useState, useReducer, useEffect } from "react";
+import { useState } from "react";
 import useFilter from "../../hooks/useFilter";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { Main } from "./styles";
 import SearchBar from "../../components/SearchBar";
 import ProjectsList from "./ProjectsList";
 import EditProject from "./EditProject";
+import ActionsBar from "./ActionsBar";
 import { IoBriefcaseOutline } from "react-icons/io5";
+import useApi from "../../hooks/useApi";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import { searchAndFilter } from "./helper";
 
 function Projects() {
+  const { state, createItem, updateItem, removeItem } = useApi("projects/");
   const [searchQuery, setSearchQuery] = useState("");
   const [filterParams, dispatch] = useFilter();
+  const [active, setActive] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleUpdate = async (id, updatedItem) => {
+    const toastId = toast.loading("Updating...");
+    try {
+      await updateItem(id, updatedItem);
+      toast.success("Item updated", { id: toastId });
+    } catch (error) {
+      toast.error("Error updating item", { id: toastId });
+    } finally {
+      setIsEditing(false);
+    }
+  };
+
+  const handleDelete = (id) => {
+    const toastId = toast.loading("Deleting...");
+    try {
+      removeItem(id);
+      toast.success("Item deleted!", { id: toastId });
+    } catch (e) {
+      toast.error(e.message, { id: toastId });
+    }
+  };
+
   return (
     <Main>
       <Toaster />
@@ -19,14 +49,29 @@ function Projects() {
         placeholder="Search projects..."
         icon={<IoBriefcaseOutline className="icon" />}
       />
-      <EditProject />
-      <ProjectsList
-        searchQuery={searchQuery}
-        filterParams={filterParams}
-        dispatch={dispatch}
+      <EditProject
+        onUpdate={handleUpdate}
+        active={active}
+        isEditing={isEditing}
+        setIsEditing={setIsEditing}
       />
+
+      <ActionsBar filterParams={filterParams} dispatch={dispatch} />
+
+      {state.loading ? (
+        <LoadingSpinner />
+      ) : (
+        <ProjectsList
+          data={searchAndFilter(state.data, searchQuery, filterParams)}
+          onDelete={handleDelete}
+          setActive={setActive}
+          setIsEditing={setIsEditing}
+        />
+      )}
     </Main>
   );
 }
 
 export default Projects;
+
+// searchAndFilter(state.data, searchQuery, filterParams)
