f2778fd0e39397fbd564a31dafcdda01da01447b
borehelu <borehelu@gmail.com>
2023-04-01 22:45:06 +0300

    include view item option

--- frontend/src/components/Options/index.js
+++ frontend/src/components/Options/index.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import { OptionsContainer, OptionsMenu } from "./styles";
 import {
 	IoCreateOutline,
 	IoTrashOutline,
 	IoEllipsisVerticalOutline,
 	IoArrowRedoOutline,
+	IoEyeOutline,
 } from "react-icons/io5";
 
-function Options({ item, onDelete, setActive, setIsEditing }) {
+function Options({ item, onDelete, setActive, setIsEditing, url }) {
 	const [show, setShow] = useState(false);
+	const navigate = useNavigate();
 
 	const handleEdit = () => {
 		setShow(false);
@@ -21,6 +24,10 @@ function Options({ item, onDelete, setActive, setIsEditing }) {
 		onDelete(item.id);
 	};
 
+	const handleView = () => {
+		navigate(`${url + item.id}`);
+	};
+
 	return (
 		<OptionsContainer>
 			<button onClick={() => setShow(!show)}>
@@ -28,6 +35,12 @@ function Options({ item, onDelete, setActive, setIsEditing }) {
 			</button>
 			{show && (
 				<OptionsMenu>
+					{url && (
+						<button onClick={handleView}>
+							<IoEyeOutline /> view
+						</button>
+					)}
+
 					<button onClick={handleEdit}>
 						<IoCreateOutline /> edit
 					</button>
