b5422ec56a6ab45aa3162ca1b469f353b3142ed9
borehelu <borehelu@gmail.com>
2023-04-02 23:52:21 +0300

    update ui for modal

--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -6,27 +6,32 @@ export const ModalOverlay = styled.div`
   bottom: 0;
   right: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.3);
   display: ${(props) => (props.show ? "flex" : "none")};
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   z-index: 1000;
 `;
 
 export const ModalContainer = styled.div`
   background-color: #fff;
-  border-radius: 8px;
+  border-radius: 4px;
   width: 80%;
   max-width: 500px;
+  margin-top: 20%;
   display: flex;
   flex-direction: column;
-  padding: 1rem 2rem;
+  padding: 0.5rem 1.5rem;
 `;
 
 export const ModalHeader = styled.div`
   display: flex;
   justify-content: space-between;
-  color: black;
+  color: #333;
+
+  & h2 {
+    font-size: 1.35rem;
+  }
 `;
 
 export const ModalBody = styled.div``;
